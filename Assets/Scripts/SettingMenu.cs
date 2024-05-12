d81b05f00f8307f81524f7259ff8ea343747fa73
Yumimel <119887308+Yumimel@users.noreply.github.com>
2024-05-12 14:40:49 +0100

    updated settings

--- Assets/Scripts/SettingMenu.cs
+++ Assets/Scripts/SettingMenu.cs
@@ -2,11 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        {
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                string option = resolutions[i].width + "x" + resolutions[i]. height;
+                options.Add(option);
+;            }
+
+        }
+
+        resolutionDropdown.AddOptions(options);
+
+    }
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("Volume", volume);
@@ -16,5 +41,9 @@ public class SettingMenu : MonoBehaviour
     {
         QualitySettings.SetQualityLevel(QualityIndex);
     }
-   
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
 }
