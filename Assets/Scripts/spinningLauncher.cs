ee6f4b69cd5d512189f54e9222780e40070fc8e5
Seatwentytwo <123640807+Seatwentytwo@users.noreply.github.com>
2024-05-15 21:56:01 +0100

    Launcher Progress
    You can test it in scene 3. It's working fine but I just need to figure out how to get the ball to launch in the direction its released in.

--- Assets/Scripts/spinningLauncher.cs
+++ Assets/Scripts/spinningLauncher.cs
@@ -6,33 +6,85 @@ public class spinningLauncher : MonoBehaviour
 {
     float lerpDuration = 0.5f;
     bool rotating;
+    bool ballCaught;
+
+    public bool directionRight;
+
+    private Collider2D collide;
+    public GameObject ballCatcher;
+    public GameObject Ball;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        collide = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(Rotating());
+        if (ballCaught == true && directionRight == true)
+        {
+            StartCoroutine(RotatingRight());
+        }
+
+        if (ballCaught == true && directionRight != true)
+        {
+            StartCoroutine(RotatingLeft());
+        }
+
+        if (ballCaught == true)
+        {
+            Ball.transform.position = transform.position;
+            collide.enabled = false;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ballCaught = false;
+            collide.enabled = false;
+        }
     }
 
-    IEnumerator Rotating()
+    IEnumerator RotatingRight()
     {
         rotating = true;
         float timeElapsed = 0;
-        Quaternion startRotation = transform.rotation;
-        Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, 0, -180);
+        Quaternion startRotation = ballCatcher.transform.rotation;
+        Quaternion targetRotation = ballCatcher.transform.rotation * Quaternion.Euler(0, 0, 180);
 
         while (timeElapsed < lerpDuration)
         {
-            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, timeElapsed / lerpDuration);
+            ballCatcher.transform.rotation = Quaternion.Slerp(startRotation, targetRotation, timeElapsed / lerpDuration);
             timeElapsed += Time.deltaTime;
             yield return null;
         }
-        transform.rotation = targetRotation;
+        ballCatcher.transform.rotation = targetRotation;
         rotating = false;
+    }
+
+    IEnumerator RotatingLeft()
+    {
+        rotating = true;
+        float timeElapsed = 0;
+        Quaternion startRotation = ballCatcher.transform.rotation;
+        Quaternion targetRotation = ballCatcher.transform.rotation * Quaternion.Euler(0, 0, -180);
+
+        while (timeElapsed < lerpDuration)
+        {
+            ballCatcher.transform.rotation = Quaternion.Slerp(startRotation, targetRotation, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        ballCatcher.transform.rotation = targetRotation;
+        rotating = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "objective")
+        {
+            ballCaught = true;
+        }
     }
 }
