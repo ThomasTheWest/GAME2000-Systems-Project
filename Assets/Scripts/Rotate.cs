d4078fd3b849de91795a4e05bb970daa8f86b9a1
Seatwentytwo <123640807+Seatwentytwo@users.noreply.github.com>
2024-05-15 16:20:45 +0100

    Rotator Improvement
    You can now only rotate in 45 degree increments. You can also use left and right mouse buttons to turn it in either direction.

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Rotate : MonoBehaviour
 {
-    float lerpDuration = 0.5f;
+    public float lerpDuration = 0.25f;
     bool rotating;
+    bool mouseOver;
 
     void OnMouseDown()
     {
@@ -16,12 +18,47 @@ public class Rotate : MonoBehaviour
         }
     }
 
+    private void OnMouseEnter()
+    {
+        mouseOver = true;
+    }
+
+    private void OnMouseExit()
+    {
+        mouseOver = false;
+    }
+
+    private void Update()
+    {
+        if (mouseOver == true && rotating != true && Input.GetMouseButtonDown(1))
+        {
+            StartCoroutine(RotateReverse90());
+        }
+    }
+
     IEnumerator Rotate90()
     {
         rotating = true;
         float timeElapsed = 0;
         Quaternion startRotation = transform.rotation;
-        Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, 0, -90);
+        Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, 0, -45);
+
+        while (timeElapsed < lerpDuration)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.rotation = targetRotation;
+        rotating = false;
+    }
+
+    IEnumerator RotateReverse90()
+    {
+        rotating = true;
+        float timeElapsed = 0;
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, 0, 45);
 
         while (timeElapsed < lerpDuration)
         {
