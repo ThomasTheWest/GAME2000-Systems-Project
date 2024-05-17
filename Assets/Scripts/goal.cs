d3176e6c588a0b61afaea3c0e14677f268a70888
Conthorp <dylanm.mcbrien@gmail.com>
2024-05-17 23:31:52 +0100

    Level change
    Goal now sends you to next level

--- Assets/Scripts/goal.cs
+++ Assets/Scripts/goal.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
-    public GameObject canvasVictory; 
+    //public GameObject canvasVictory; 
     public string tagObjective = "objective";
+
+    public int sceneIndex;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag(tagObjective))
         {
-            canvasVictory.SetActive(true);
-            Debug.Log("You win!");
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
         }
     }
 }
